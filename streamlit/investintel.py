49fdc12bfc3df110ca874f676976eecfdb24da2a
surya valluri <suryavalluri@nseit.com>
2024-09-11 16:18:01 +0530

    added single chat page

--- streamlit/investintel.py
+++ streamlit/investintel.py
@@ -1,43 +1,68 @@
 import streamlit as st
 from snowflake.snowpark import Session
-from cortexCaller import CortexCaller
+from cortexCaller import call_complete
 import snowflake.permissions as permission
+
+st.title('Investintel', anchor=False)
 
 class Dashboard:
    
-   def __init__(self, session: Session, cortex_caller: CortexCaller = CortexCaller()) -> None:
-      self.session = session
-      self.cortex_caller = cortex_caller
+    def __init__(self, session: Session) -> None:
+        self.session = session
 
-   def setup(self):
-      st.header("Privileges setup")
-      st.caption("""
+    def setup(self):
+        st.header("Privileges setup")
+        st.caption("""
          Follow the instructions below to set up your application.
          Once you have completed the steps, you will be able to continue to the main example.
       """)
-      privilege = 'IMPORTED PRIVILEGES ON SNOWFLAKE DB'
-      if not permission.get_held_account_privileges([privilege]):
-         st.button(f"Grant import privileges on snowflake DB ↗", on_click=permission.request_account_privileges, args=[[privilege]], key='IMPORTED PRIVILEGES ON SNOWFLAKE DB')
-      else:
-         st.session_state.privileges_granted = True
-         st.rerun()
+        privilege = 'IMPORTED PRIVILEGES ON SNOWFLAKE DB'
+        if not permission.get_held_account_privileges([privilege]):
+            st.button(f"Grant import privileges on snowflake DB ↗", on_click=permission.request_account_privileges, args=[[privilege]], key='IMPORTED PRIVILEGES ON SNOWFLAKE DB')
+        else:
+            st.session_state.privileges_granted = True
+            st.rerun()
 
-   def run_streamlit(self):
-      if not permission.get_held_account_privileges(['IMPORTED PRIVILEGES ON SNOWFLAKE DB']):
-         del st.session_state.privileges_granted
-         st.rerun()
-      
-      st.header('Snowflake Cortex Example')
-      st.subheader("Simple app showing how cortex can answer questions using the following song's ranking table")
-      table_df = self.session.table("SHARED_DATA.NAV_DATA").to_pandas()
-      st.write(table_df)
+    def run_streamlit(self):
+        if not permission.get_held_account_privileges(['IMPORTED PRIVILEGES ON SNOWFLAKE DB']):
+            del st.session_state.privileges_granted
+            st.rerun()
+       
+        instructions = "Be concise. Do not hallucinate"
 
-      cortex_chat = st.container(height=200)
-      cortex_input = st.chat_input("What do you want to ask to cortex about the table from above?", key="cortex_input")
-      if cortex_input and not cortex_input.isspace():
-         cortex_chat.chat_message("user").write(cortex_input)
-         cortex_fn = self.cortex_caller.call_cortex(table_df, cortex_input)
-         cortex_chat.chat_message("assistant").write(cortex_fn)
+        if "messages" not in st.session_state:
+            st.session_state["messages"] = [
+                {
+                    'role': 'assistant',
+                    'content': 'how can I help?'
+                }
+            ]
+
+        for message in st.session_state.messages:
+            with st.chat_message(message['role']):
+                st.markdown(message['content'])
+
+
+        prompt = st.chat_input("Type your message")
+
+        if prompt:
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            
+            with st.chat_message("assistant"):  
+                context = ",".join(f"role:{message['role']} content:{message['content']}" for message in st.session_state.messages)
+                #response = Complete(model, f"Instructions:{instructions}, Context:{context}, Prompt:{prompt}") 
+                input = f"Instructions:{instructions}, Context:{context}, Prompt:{prompt}"
+                response = call_complete(input)
+                st.markdown(response)
+            
+                st.session_state.messages.append({
+                    'role': 'assistant',
+                    'content': response
+                })
+
 
 
 if __name__ == '__main__':
